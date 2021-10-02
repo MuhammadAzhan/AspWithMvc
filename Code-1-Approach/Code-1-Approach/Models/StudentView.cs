005e6a50a630eb7c1014070de27dad9d643d0c96
MuhammadAzhan <azhan2.hp@gmail.com>
2021-10-02 20:58:21 +0500

    Code First Approach

---
+++ Code-1-Approach/Code-1-Approach/Models/StudentView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Code_1_Approach.Models
+{
+    public class StudentView
+    {
+        public List<Student> getAllRecord()
+        {
+            StudentContext obj = new StudentContext();
+            return obj.students.ToList();
+
+        }
+    }
+}
